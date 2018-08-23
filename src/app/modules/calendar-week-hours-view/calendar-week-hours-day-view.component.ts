8a6c0143f54156c51fb91c7b98e5cedf97242556
Ignacio Vissani <ivissani@gmail.com>
2018-08-22 21:18:26 -0300

    Stronger try for fixing ECAIHBC

--- src/app/modules/calendar-week-hours-view/calendar-week-hours-day-view.component.ts
+++ src/app/modules/calendar-week-hours-view/calendar-week-hours-day-view.component.ts
@@ -331,7 +331,7 @@ export class CalendarWeekHoursDayViewComponent
             changes.eventWidth
         ) {
             this.refreshView();
-            this.cdr.markForCheck();
+            this.cdr.detectChanges();
         }
     }
 
